afe0f8edf4f2286a254ff29713d0fc51289a6385
Martin Evans <martindevans@gmail.com>
2014-12-27 15:28:39 +0000

    Covered variable length decoding with tests, discovered the bug in my implementation.
    Signed-off-by: Martin Evans <martindevans@gmail.com>

--- MumbleSharp/UdpPacketReader.cs
+++ MumbleSharp/UdpPacketReader.cs
@@ -20,6 +20,9 @@ namespace MumbleSharp
 
         public byte ReadByte()
         {
+            if (_inner.Position >= _inner.Length)
+                throw new EndOfStreamException("Cannot read any more bytes from Udp Packet");
+
             return (byte)_inner.ReadByte();
         }
 
@@ -35,80 +38,50 @@ namespace MumbleSharp
 
         public long ReadVarInt64()
         {
-            ////My implementation, neater (imo) but broken
-            //byte b = ReadByte();
-            //int leadingOnes = LeadingOnes(b);
-            //switch (leadingOnes)
-            //{
-            //    case 0:
-            //        return b & 127;
-            //    case 1:
-            //        //10xxxxxx + 1 byte
-            //        return ((b & 63) << 8) | ReadByte();
-            //    case 2:
-            //        //110xxxxx + 2 bytes
-            //        return ((b & 31) << 16) | ReadByte() << 8 | ReadByte();
-            //    case 3:
-            //        //1110xxxx + 3 bytes
-            //        return ((b & 15) << 24) | ReadByte() << 16 | ReadByte() << 8 | ReadByte();
-            //    case 4:
-            //        // Either:
-            //        //  > 111100__ + int (4 bytes)
-            //        //  > 111101__ + long (8 bytes)
-            //        if ((b & 4) == 4)
-            //        {
-            //            //111101__ + long (8 bytes)
-            //            return ReadByte() << 56 | ReadByte() << 48 | ReadByte() << 40 | ReadByte() << 32 | ReadByte() << 24 | ReadByte() << 16 | ReadByte() << 8 | ReadByte();
-            //        }
-            //        else
-            //        {
-            //            //111100__ + int (4 bytes)
-            //            return ReadByte() << 24 | ReadByte() << 16 | ReadByte() << 8 | ReadByte();
-            //        }
-            //    case 5:
-            //        //111110 + varint (negative)
-            //        return -ReadVarInt64();
-            //    case 6:
-            //        //111111xx Byte-inverted negative two bit number (~xx)
-            //        return ~(b & 3);
-            //    default:
-            //        throw new InvalidDataException("Invalid varint encoding");
-            //}
+            //My implementation, neater (imo) but broken
+            byte b = ReadByte();
+            int leadingOnes = LeadingOnes(b);
+            switch (leadingOnes)
+            {
+                case 0:
+                    return b & 127;
+                case 1:
+                    //10xxxxxx + 1 byte
+                    return ((b & 63) << 8) | ReadByte();
+                case 2:
+                    //110xxxxx + 2 bytes
+                    return ((b & 31) << 16) | ReadByte() << 8 | ReadByte();
+                case 3:
+                    //1110xxxx + 3 bytes
+                    return ((b & 15) << 24) | ReadByte() << 16 | ReadByte() << 8 | ReadByte();
+                case 4:
+                    // Either:
+                    //  > 111100__ + int (4 bytes)
+                    //  > 111101__ + long (8 bytes)
+                    if ((b & 4) == 4)
+                    {
+                        //111101__ + long (8 bytes)
+                        return ReadByte() << 56 | ReadByte() << 48 | ReadByte() << 40 | ReadByte() << 32 | ReadByte() << 24 | ReadByte() << 16 | ReadByte() << 8 | ReadByte();
+                    }
+                    else
+                    {
+                        //111100__ + int (4 bytes)
+                        return ReadByte() << 24 | ReadByte() << 16 | ReadByte() << 8 | ReadByte();
+                    }
+                case 5:
+                    //111110 + varint (negative)
+                    return ~ReadVarInt64();
+                case 6:
+                case 7:
+                case 8:
+                    //111111xx Byte-inverted negative two bit number (~xx)
 
-            //From https://github.com/mumble-voip/mumble/blob/master/src/PacketDataStream.h
-            long i = 0;
-		    long v = ReadByte();
-
-		    if ((v & 0x80) == 0x00) {
-			    i = v & 0x7F;
-		    } else if ((v & 0xC0) == 0x80) {
-			    i = (v & 0x3F) << 8 | ReadByte();
-		    } else if ((v & 0xF0) == 0xF0) {
-                switch (v & 0xFC)
-                {
-			        case 0xF0:
-                            i = ReadByte() << 24 | ReadByte() << 16 | ReadByte() << 8 | ReadByte();
-				        break;
-			        case 0xF4:
-                        i = ReadByte() << 56 | ReadByte() << 48 | ReadByte() << 40 | ReadByte() << 32 |
-                            ReadByte() << 24 | ReadByte() << 16 | ReadByte() << 8 | ReadByte();
-				        break;
-			        case 0xF8:
-				        i = ~ReadVarInt64();
-				        break;
-			        case 0xFC:
-				        i = v & 0x03;
-				        i = ~i;
-				        break;
-			        default:
-                        throw new InvalidDataException("Invalid varint encoding");
-			    }
-		    } else if ((v & 0xF0) == 0xE0) {
-                i = (v & 0x0F) << 24 | ReadByte() << 16 | ReadByte() << 8 | ReadByte();
-		    } else if ((v & 0xE0) == 0xC0) {
-                i = (v & 0x1F) << 16 | ReadByte() << 8 | ReadByte();
-		    }
-		    return i;
+                    //We need three cases here because all the other leading parts are capped off by a zero, e.g. 11110xxx
+                    //However in this case it's just 6 ones, and then the data (111111xx). Depending on the data, the leading count changes
+                    return ~(b & 3);
+                default:
+                    throw new InvalidDataException("Invalid varint encoding");
+            }
         }
 
         private static readonly byte[] _leadingOnesLookup = {
--- MumbleSharpTest/UdpPacketReaderTest.cs
+++ MumbleSharpTest/UdpPacketReaderTest.cs
@@ -31,5 +31,118 @@ namespace MumbleSharpTest
 
             
         }
+
+        private static byte B(string s)
+        {
+            return Convert.ToByte(s, 2);
+        }
+
+        private static UdpPacketReader R(params byte[] bytes)
+        {
+            return new UdpPacketReader(new MemoryStream(bytes));
+        }
+
+        [TestMethod]
+        public void VariableLength_ZeroLeadingOnes()
+        {
+            var expected = B("01001001");
+
+            UdpPacketReader r = R(expected, B("11111111"));
+
+            var actual = r.ReadVarInt64();
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void VariableLength_OneLeadingOne()
+        {
+            var expected = B("00101100") << 8 | B("11100101");
+
+            UdpPacketReader r = R(B("10101100"), B("11100101"), B("11111111"));
+
+            var actual = r.ReadVarInt64();
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void VariableLength_TwoLeadingOnes()
+        {
+            var expected = B("00010010") << 16 | B("11100101") << 8 | B("11111111");
+
+            UdpPacketReader r = R(B("11010010"), B("11100101"), B("11111111"));
+
+            var actual = r.ReadVarInt64();
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void VariableLength_ThreeLeadingOnes()
+        {
+            var expected = B("00000010") << 24 | B("11100101") << 16 | B("11111111") << 8 | B("00001111");
+
+            UdpPacketReader r = R(B("11100010"), B("11100101"), B("11111111"), B("00001111"));
+
+            var actual = r.ReadVarInt64();
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void VariableLength_FourLeadingOnes_FourBytes()
+        {
+            var expected = B("11100101") << 24 | B("11111111") << 16 | B("00001111") << 8 | B("10101010");
+
+            UdpPacketReader r = R(B("11110010"), B("11100101"), B("11111111"), B("00001111"), B("10101010"));
+
+            var actual = r.ReadVarInt64();
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void VariableLength_FourLeadingOnes_EightBytes()
+        {
+            var expected = B("11100101") << 56 | B("11111111") << 48 | B("00001111") << 40 | B("10101010") << 32 | B("11100101") << 24 | B("11111111") << 16 | B("00001111") << 8 | B("10101010");
+
+            UdpPacketReader r = R(B("11110110"), B("11100101"), B("11111111"), B("00001111"), B("10101010"), B("11100101"), B("11111111"), B("00001111"), B("10101010"));
+
+            var actual = r.ReadVarInt64();
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void VariableLength_Negative()
+        {
+            var expected = ~(B("11100101") << 24 | B("10000001") << 16 | B("00001111") << 8 | B("10101010"));
+
+            UdpPacketReader r = R(B("11111000"), B("11110010"), B("11100101"), B("10000001"), B("00001111"), B("10101010"));
+
+            var actual = r.ReadVarInt64();
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void VariableLength_InvertedTwoBitNumber()
+        {
+            //These are 2 bit numbers, test all 4 possibilities
+
+            var expected = ~B("00000000");
+            var r = R(B("11111100"));
+            var actual = r.ReadVarInt64();
+            Assert.AreEqual(expected, actual);
+
+            expected = ~B("00000001");
+            r = R(B("11111101"));
+            actual = r.ReadVarInt64();
+            Assert.AreEqual(expected, actual);
+
+            expected = ~B("00000010");
+            r = R(B("11111110"));
+            actual = r.ReadVarInt64();
+            Assert.AreEqual(expected, actual);
+
+            expected = ~B("00000011");
+            r = R(B("11111111"));
+            actual = r.ReadVarInt64();
+            Assert.AreEqual(expected, actual);
+        }
     }
 }
